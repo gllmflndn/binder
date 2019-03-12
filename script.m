47658aa758670b01eb52a08a003d3b6954fda9c1
Guillaume <gflandin@localhost.localdomain>
2019-03-12 15:37:34 +0000

    Coregistration script

--- script.m
+++ script.m
@@ -1,10 +1,6 @@
-%-Coregistration with SPM12
-%-----------------------------------------------------------
 
 wd = '/home/jovyan';
-
 addpath(fullfile(wd,'src'));
-
 cd(fullfile(wd,'data'));
 
 %-Input data
@@ -13,6 +9,10 @@ src = fullfile(wd,'data','bold.nii');
 
 %-Reslice (for display...)
 spm_reslice({ref,src}, struct('mean',false,'which',1));
+
+A = spm_slice_vol(spm_vol('T1w.nii'),spm_matrix([0 0 80]),[99 134],1);
+F = spm_slice_vol(spm_vol('rbold.nii'),spm_matrix([0 0 80]),[99 134],1);
+subplot(1,2,1),imagesc(rot90(A)),axis equal,axis tight,subplot(1,2,2),imagesc(rot90(F)),axis equal, axis tight
 
 %-Coregister
 x = spm_coreg(ref, src);
@@ -24,3 +24,7 @@ spm_get_space(src, X\M);
 
 %-Reslice
 spm_reslice({ref,src}, struct('mean',false,'which',1));
+
+A = spm_slice_vol(spm_vol('T1w.nii'),spm_matrix([0 0 80]),[99 134],1);
+F = spm_slice_vol(spm_vol('rbold.nii'),spm_matrix([0 0 80]),[99 134],1);
+subplot(1,2,1),imagesc(rot90(A)),axis equal,axis tight,subplot(1,2,2),imagesc(rot90(F)),axis equal, axis tight
